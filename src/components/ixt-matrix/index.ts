80824f5a97b4b14c74899c31ae77b7982b0826ec
Van Auken <DVANAUK@citgo.com>
2024-12-03 09:38:04 -0600

    Pre-release commit

--- src/components/ixt-matrix/index.ts
+++ src/components/ixt-matrix/index.ts
@@ -1,7 +1,8 @@
 // src/components/ixt-matrix/index.ts
 // Fix these import paths to match your folder structure
 export * from './ixt-matrix.component';
-export * from './ixt-matrix.module';
+export * from './ixt-matrix.interfaces';
+export * from './ixt-matrix.module';export * from './ixt-matrix.module';
 export interface MatrixNode {
     [key: string]: any;
     children?: MatrixNode[];
